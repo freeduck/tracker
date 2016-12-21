ccd343ccfdc7fe862125827b9f2a45976be7dc74
Lars Hostrup <larshope@gmail.com>
2016-12-21 23:43:08 +0100

    count

--- newtracker.py
+++ newtracker.py
@@ -15,14 +15,15 @@ img1.addDrawingLayer(balllayer)
 
 
 def main():
-    findBorders()
-    track()
+    track(findBorders())
 
 def findBorders():
     continue_looking = True
+    rectangles = True
     while continue_looking and display.isNotDone():
         if display.mouseLeft:
                 print "Right"
+                layer.clear()
                 break
         layer.clear()
         img = cam.getImage().flipHorizontal()
@@ -38,11 +39,25 @@ def findBorders():
         img1.applyLayers()
         img1.show()
 
+    print rectangles
+    return rectangles
 
 
-def track():
-        while True:
-            #dislay.isNotDone():
+
+def track(rectangles):
+    l = []
+    for idx,rect in enumerate(rectangles):
+                layer.rectangle((rect.x, 0), (5, img1.height),SimpleCV.Color.BLUE )
+                l.insert(idx,rect)
+
+    leftb = l[0].x
+    rightb = l[1].x
+    prevcircle = None
+    count = 0
+    circle = Noneb
+    balllayer.setFontSize(24)
+
+    while True:
             balllayer.clear()
             img =  cam.getImage().flipHorizontal()
             dist = img.colorDistance(SimpleCV.Color.BLACK).dilate(2)
@@ -53,8 +68,12 @@ def track():
                     circles = blobs.filter([b.isCircle(0.6) for b in blobs])
                     if circles:
                         circle=circles[-1]
+                        if prevcircle and circle.x < leftb and prevcircle.x >= leftb:
+                            count = count+1
 
 
+                        prevcircle=circle
+            balllayer.text(str(count),(10,10),color=SimpleCV.Color.GREEN)
 
             balllayer.circle((circle.x, circle.y), 20,SimpleCV.Color.RED,3)
             img1.applyLayers()
